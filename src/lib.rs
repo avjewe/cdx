8a1eef7e2f2e6500b61bb72f83d32d81457bc0f9
Andy Jewell <ajewell@amazon.com>
2021-11-07 16:01:00 -0500

    m

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,23 @@
 //! and command line data mining.
 //! It is hoped that the associated library will be useful for theid party tools.
 //!
-//! Tools :
+//! # Tools :
 //! - cgrep : select lines of text from a file
 //! - cut : rearrange columns within lines of text
 //! - join : combine text files by macthing column values
 //! - sort : rearrange lines within files
 //! - tooltest : test command line programs
 //! - uniq : remove duplicate lines
-
+//!
+//! # Modules
+//! - lib : file i/o Error and Result, basic types
+//! - check : Does a line match a pattern
+//! - column : working with named columns, and groups thereof
+//! - comp : How do two lines compare to each other
+//! - expr : floating point expressions, optimized to re-evaluate many times
+//! - join : combine text files by macthing column values
+//! - sort : sort files
+//! - tooltest : test command line programs
 #![warn(
     missing_copy_implementations,
     absolute_paths_not_starting_with_crate,
